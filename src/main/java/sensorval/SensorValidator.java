802a00f7bb10cf9a5efd1f2713dc1fdd283f41cf
PraveenDharmaraj92 <62659035+PraveenDharmaraj92@users.noreply.github.com>
2021-03-25 19:41:57 +0530

    Reduced complexity

--- src/main/java/sensorval/SensorValidator.java
+++ src/main/java/sensorval/SensorValidator.java
@@ -7,35 +7,53 @@ import java.util.List;
  *
  */
 public class SensorValidator {
-	
+
 	/**
-	 * @param value - value 1
-	 * @param nextValue - value 2, sequential to value 1
-	 * @param maxDelta - maximum deviation allowed
-	 * @return - true if difference between 2 values is not more than delta value 
+	 * @param value
+	 *            - value 1
+	 * @param nextValue
+	 *            - value 2, sequential to value 1
+	 * @param maxDelta
+	 *            - maximum deviation allowed
+	 * @return - true if difference between 2 values is not more than delta
+	 *         value
 	 */
 	public static boolean isReadingNormal(double value, double nextValue, double maxDelta) {
 		return (nextValue - value) < maxDelta;
 	}
 
 	/**
-	 * @param values - list of values
-	 * @param maxDelta - maximum deviation allowed 
-	 * @return  true if no deviations in readings
+	 * @param values
+	 *            - list of values
+	 * @param maxDelta
+	 *            - maximum deviation allowed
+	 * @return true if no deviations in readings
 	 */
 	public static boolean validateReadings(List<Double> values, Double maxDelta) {
+		return !isNullorEmpty(values) && !isDeltaValueBreached(values, maxDelta);
+	}
 
-		if (values == null||values.isEmpty()) {
-			return false;
-		}
+	/**
+	 * @param values - value list
+	 * @return true if null and not empty
+	 */
+	public static boolean isNullorEmpty(List<Double> values) {
+		return values == null || values.isEmpty();
+	}
 
+	/**
+	 * @param values - value list
+	 * @param maxDelta - maximum deviation allowed
+	 * @return  true if deviations are found in readings
+	 */
+	public static boolean isDeltaValueBreached(List<Double> values, Double maxDelta) {
 		int lastButOneIndex = values.size() - 1;
 		for (int i = 0; i < lastButOneIndex; i++) {
 			if (!isReadingNormal(values.get(i), values.get(i + 1), maxDelta)) {
-				return false;
+				return true;
 			}
 		}
-		return true;
+		return false;
 	}
 
 }
